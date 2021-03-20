a1ea1f9e0d04934edbde219a9c8c1f5633cf2489
Vladislav Kochkin <ltrickfaim@gmail.com>
2021-03-20 16:14:01 +0600

    #10 - Добавил роуты для логина - регистрации.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,40 @@
+import React from "react";
 import './App.css';
+import { Switch, Route } from "react-router-dom";
+import { Redirect } from "react-router";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Layout from "./components/Layout/Layout";
+import LoginRegisterPage from "./containers/LoginRegisterPage/LoginRegisterPage";
+import {useSelector} from "react-redux";
+
+const ProtectedRoute = ({isAllowed, redirectTo, ...props}) => {
+    return isAllowed ? (<Route {...props} />)
+        : (<Redirect to={redirectTo} />
+    );
+};
+
 
 function App() {
+    const {user} = useSelector(state => state.users);
   return (
-    <div className="App">
-      <h1>Hello!</h1>
-    </div>
+    <Layout>
+        <ToastContainer autoClose={3000} position='top-center' />
+        <Switch>
+            <ProtectedRoute
+                path={`/login`}
+                component={LoginRegisterPage}
+                isAllowed={!user}
+                redirectTo='/'
+            />
+            <ProtectedRoute
+                path={`/register`}
+                component={LoginRegisterPage}
+                isAllowed={!user}
+                redirectTo='/'
+            />
+        </Switch>
+    </Layout>
   );
 }
 
